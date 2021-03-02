6165ab5389c87324791dffe2d6990f843bdf57a5
Konstantinos Pap <63195954+Konstantinos-Papanagnou@users.noreply.github.com>
2021-03-02 19:45:47 +0200

    Test Comment By Konstantinos Pap

--- src/application/Main.java
+++ src/application/Main.java
@@ -5,7 +5,7 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
-
+// Test Comment by Author
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
